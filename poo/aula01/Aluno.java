4f6906ddaf91806c8f5bd401cd390c53a5a89320
Ayla Lopes <163359268+taengooni@users.noreply.github.com>
2025-02-21 22:27:31 +0000

    oi

---
+++ poo/aula01/Aluno.java
@@ -0,0 +1,11 @@
+public class Aluno {
+    public String nome;
+    public double altura;
+
+    public void estudar() {
+        System.out.println("ayla está estudando poo");
+    }
+    public void pedirNota() {
+        System.out.println("ayla está pedindo nota para iago");
+}
+}
