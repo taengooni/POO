2ef6b835df82b747e848516040e92db40ce1620f
Ayla Lopes <163359268+taengooni@users.noreply.github.com>
2025-02-21 22:59:59 +0000

    oi

--- poo/aula01/src/App.java
+++ poo/aula01/src/App.java
@@ -1,5 +1,16 @@
+import aula01.Aluno;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Aluno aluno1 = new Aluno(); // nova instancia; novo objeto da classe
+aluno1.nome = "Jansen";
+aluno1.altura = 1.65;
+System.out.println("Nome do aluno " + aluno1.nome);
+System.out.println("Altura do aluno " + aluno1.altura);
+
+aluno1.nome = "Ayla";
+System.out.println("Nome do aluno " + aluno1.nome);
+
+aluno1.estudar();
     }
 }
